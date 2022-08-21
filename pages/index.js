7352fd9a7119ff928ae2ee07e77741b82abb3cf1
archiegalivo <aagalivo.student@ua.edu.ph>
2022-08-21 15:10:40 +0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,20 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>Hello, I'm <b>Archie</b>. I'm a 4th year Computer Enginnering student from University of the Assumption. My hobbies anything related to computer literate. Also my favorite food is Asado and anything about myself is personality. You can contact me on <Link href="https://www.facebook.com/archie.galivo.75/">Facebook.</Link></p>
+        <p>
+          (This is a sample website - you’ll be building a site like this on{' '}
+          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        </p>
+      </section>
+    </Layout>
+  );
+}
